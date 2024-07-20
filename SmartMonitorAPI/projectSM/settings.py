a6fd7091b92068fc5a144240adc461bf73ff5dcf
JohnLenon22 <joonleno122213@gmail.com>
2024-07-20 00:41:31 -0300

    implementei o swagger

--- SmartMonitorAPI/projectSM/settings.py
+++ SmartMonitorAPI/projectSM/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appSM',
-    'djangorestframework',
+    'rest_framework',
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
