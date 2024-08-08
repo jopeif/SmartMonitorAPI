989133136e82978e2f22c746fea4cd1c72e61eb4
John <joonleno122213@gmail.com>
2024-08-07 23:24:11 -0300

    token access é valido por 1 dia

--- projectSM/settings.py
+++ projectSM/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
--- projectSM/urls.py
+++ projectSM/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('RandomForest/', Pred_RandomForest.as_view()),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
