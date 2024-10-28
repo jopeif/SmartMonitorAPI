d4d58ef2651ef6b61bb318ba3dec607c38afed90
John <joonleno122213@gmail.com>
2024-10-28 11:24:41 -0300

    adicionado o XGBooster

--- modelosML/RandomForest/carregar_joblib.py
+++ modelosML/RandomForest/carregar_joblib.py
@@ -1,39 +1,40 @@
 import numpy as np
 import pandas as pd
+from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import joblib
 
 # Carregar os dados
-Base_Dados = pd.read_excel('modelosML/Consumos/RandomForest/dados_diario WEBER LUCAS.xlsx')
-
-# Remover valores nulos
+Base_Dados = pd.read_excel('./modelosML/Consumos/dados_diario WEBER LUCAS.xlsx')
 Base_Dados.dropna(inplace=True)
 
-# Separar features e target
-x = Base_Dados.iloc[:, :1]  # Primeira coluna como feature
-y = Base_Dados.iloc[:, 1]   # Segunda coluna como target
+consumo = Base_Dados['CONSUMO'].values
 
-# Dividir os dados em treino e teste
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=66)
+def create_features_targets(data, window_size=30):
+    features, targets = [], []
+    for i in range(len(data) - window_size):
+        features.append(data[i:i + window_size])
+        targets.append(data[i + window_size])
+    return np.array(features), np.array(targets)
 
-# Treinar o modelo
-modelo = RandomForestRegressor(n_estimators=1000, random_state=42)
+x, y = create_features_targets(consumo, window_size=30)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=122)
+
+modelo = XGBRegressor(use_label_encoder=False, eval_metric='mlogloss')
 modelo.fit(x_train, y_train)
 
-# Fazer previsões no conjunto de teste
 previsor = modelo.predict(x_test)
 
-# Função para prever o próximo número com base nos últimos 30 números
-def predict_next_number(model, last_30_numbers):
-    last_30_numbers = np.array(last_30_numbers).reshape(1, 30)  # Ajustar para formato (1, 30)
-    return model.predict(last_30_numbers)[0]
+def predict_next_number(model, last_numbers, window_size=30):
+      last_numbers = np.array(last_numbers).reshape(1, -1)
+      return model.predict(last_numbers)[0]
 
 # Exemplo de uso da função
 # Obtendo os últimos 30 valores de y para prever o próximo valor
-last_30_numbers = y.tail(30).values
+last_30_numbers = y[-30:]
 predicted_next_number = predict_next_number(modelo, last_30_numbers)
 print("Predição do trigésimo primeiro número:", predicted_next_number)
 
 # Salvar o modelo
-#joblib.dump(modelo, 'modelosML/RandomForest/modeloPreverConsumo.joblib')
+joblib.dump(modelo, 'modelosML/RandomForest/modeloPreverConsumo.joblib')
--- modelosML/RandomForest/randomforest.py
+++ modelosML/RandomForest/randomforest.py
@@ -1,19 +1,16 @@
 import numpy as np
 import pandas as pd
+from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import joblib
 
-
 # Carregar os dados
-Base_Dados = pd.read_excel('modelosML/Consumos/dados_diario WEBER LUCAS.xlsx')
-
-# Remover valores nulos
+Base_Dados = pd.read_excel('/content/dados_diario WEBER LUCAS.xlsx')
 Base_Dados.dropna(inplace=True)
 
 consumo = Base_Dados['CONSUMO'].values
 
-# Criar a série temporal de features e target
 def create_features_targets(data, window_size=30):
     features, targets = [], []
     for i in range(len(data) - window_size):
@@ -22,22 +19,20 @@ def create_features_targets(data, window_size=30):
     return np.array(features), np.array(targets)
 
 x, y = create_features_targets(consumo, window_size=30)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=122)
 
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=66)
-
-modelo = RandomForestRegressor(n_estimators=1000, random_state=42)
+modelo = XGBRegressor(use_label_encoder=False, eval_metric='mlogloss')
 modelo.fit(x_train, y_train)
 
 previsor = modelo.predict(x_test)
 
-def predict_next_number(model, last_30_numbers):
-    last_30_numbers = np.array(last_30_numbers).reshape(1, 30)
-    return model.predict(last_30_numbers)[0]
+def predict_next_number(model, last_numbers, window_size=30):
+      last_numbers = np.array(last_numbers).reshape(1, -1)
+      return model.predict(last_numbers)[0]
 
 # Exemplo de uso da função
-# Obtendo os últimos 30 valores de consumo
-last_30_numbers = consumo[-30:]
+# Obtendo os últimos 30 valores de y para prever o próximo valor
+last_30_numbers = y.tail(30).values
 predicted_next_number = predict_next_number(modelo, last_30_numbers)
 print("Predição do trigésimo primeiro número:", predicted_next_number)
 
