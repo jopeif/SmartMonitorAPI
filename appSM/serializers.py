25d75fbd1157c18b65f407f65b70ec1a104252d2
John <joonleno122213@gmail.com>
2025-01-27 09:57:34 -0300

    Validar data no serializer e troca do modelo xgboost pelo Rf regressor

--- appSM/serializers.py
+++ appSM/serializers.py
@@ -1,8 +1,27 @@
-
+from datetime import datetime
 from rest_framework import serializers
 
 class MySerializer(serializers.Serializer):
-    data = serializers.ListField(
-        child=serializers.FloatField()
-    )       
+    def to_internal_value(self, data):
+         
+        """
+        Valida o formato esperado onde as chaves são datas 'DD/MM/YYYY' 
+        e os valores são float ou null.
+        """
+        if not isinstance(data, dict):
+            raise serializers.ValidationError("Os dados devem ser um dicionário.")
+        
+        for key, value in data.items():
+            # Valida a chave como uma data no formato 'DD/MM/YYYY'
+            try:
+                datetime.strptime(key, '%d/%m/%Y')
+            except ValueError:
+                raise serializers.ValidationError(f"A chave '{key}' não está no formato DD/MM/YYYY.")
+            
+            # Valida que o valor seja float ou None
+            if value is not None and not isinstance(value, (float, int)):
+                raise serializers.ValidationError(f"O valor para a chave '{key}' deve ser um número ou null.")
+        
+        return super().to_internal_value(data)
+        
  
--- modelosML/RandomForest/Test_month/main.py
+++ modelosML/RandomForest/Test_month/main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pandas as pd
-from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 def train_model():
+    Base_Dados = pd.read_excel('./modelosML/Consumos/IFCE - Unidades.xlsx')
+    Base_Dados.sort_values(by='DATA', inplace=True)
+    consumo = Base_Dados['CONSUMO'].values
 
-    Base_Dados = pd.read_excel('./modelosML/Consumos/consumo_agua.xlsx')
-    Base_Dados.dropna(inplace=True)
-    consumo = Base_Dados['Consumo(m³)'].values
-
+    # Criar a série temporal de features e target
     def create_features_targets(data, window_size=12):
         features, targets = [], []
         for i in range(len(data) - window_size):
@@ -17,56 +18,25 @@ def train_model():
         return np.array(features), np.array(targets)
 
     x, y = create_features_targets(consumo, window_size=12)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=122)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=66)
 
-    modelo = XGBRegressor(
-        use_label_encoder=False,
-        eval_metric='rmse',  # Para regressão, "rmse" é uma métrica adequada
-        n_estimators=100,  # Número de árvores
-        max_depth=3,  # Profundidade máxima das árvores
-        learning_rate=0.01,  # Taxa de aprendizado
-        subsample=0.8,  # Subamostragem de dados
-        min_child_weight=3,  # Peso mínimo para folhas das árvores
-        gamma=0.5,  # Regularização para evitar overfitting
-        colsample_bytree=0.7  # Subamostragem de colunas para cada árvore
-    )
+    modelo = RandomForestRegressor(n_estimators=500, random_state=42)
     modelo.fit(x_train, y_train)
-    return modelo
-    # previsor = modelo.predict(x_test)
 
-def predict_next_month(modelo, last_numbers, window_size=12):
+    y_pred = modelo.predict(x_test)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    print(f"RMSE no conjunto de teste: {rmse}")
+
+    return modelo
+
+def predict_next_day(modelo, last_numbers, window_size=12):
+    
+    last_numbers = pd.read_excel('./modelosML/Consumos/IFCE - Unidades.xlsx', 'Tabuleiro do Norte')
+    
     if len(last_numbers) != window_size:
         raise ValueError(f"O tamanho de last_numbers deve ser {window_size}, mas é {len(last_numbers)}.")
     
-    last_numbers = np.array(last_numbers).reshape(1, -1)
+    last_numbers = np.array(last_numbers).astype(float).reshape(1, -1)
     return modelo.predict(last_numbers)[0]
 
-model_trained_month = train_model()
- 
-
-
-
-
-##*Avaliar para achar melhorar parametros*##
-# from sklearn.model_selection import RandomizedSearchCV
-# import numpy as np
-
-# # Definir o grid de parâmetros para busca aleatória
-# param_dist = {
-#     'n_estimators': [100, 200, 300, 500],
-#     'learning_rate': [0.01, 0.05, 0.1, 0.3],
-#     'max_depth': [3, 5, 7, 9],
-#     'subsample': [0.7, 0.8, 0.9, 1.0],
-#     'colsample_bytree': [0.7, 0.8, 1.0],
-#     'gamma': [0, 0.1, 0.3, 0.5],
-#     'min_child_weight': [1, 3, 5],
-# }
-
-# # Usar RandomizedSearchCV
-# random_search = RandomizedSearchCV(estimator=modelo, param_distributions=param_dist, 
-#                                 n_iter=100, scoring='neg_mean_squared_error', 
-#                                 cv=5, n_jobs=-1, verbose=2, random_state=42)
-# random_search.fit(x_train, y_train)
-
-# # Exibir os melhores parâmetros
-# print("Melhores parâmetros:", random_search.best_params_)
+model_trained_day = train_model()
