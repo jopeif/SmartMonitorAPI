3e4d73deaba0ffa46fdb0c91a1ddc5ea015a50a1
douglasnobree <douglasclaudino543@gmail.com>
2025-02-13 11:10:21 -0300

    fix: :bug: ativar modo debug para desenvolvimento

--- projectSM/settings.py
+++ projectSM/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rd#w@4sls_%$4=m90xt6#%_m+u__#*dx1m0+r@f4!d4iahn(=a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
