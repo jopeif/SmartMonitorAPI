81b4179aff5d42696700c625ce46e5032589f432
douglasnobree <douglasclaudino543@gmail.com>
2025-02-16 15:48:27 -0300

    fix: atualizar caminho do Node.js no script de pós-deploy do ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'django-api',
-      script: '/home/jarbas/.virtualenvs/django-env/bin/gunicorn',  // Caminho absoluto para o gunicorn no ambiente virtual
+      script: '/home/jarbas/.virtualenvs/django-env/bin/gunicorn', // Caminho absoluto para o gunicorn no ambiente virtual
       args: 'config.wsgi:application --bind 0.0.0.0:8000 --workers 3',
       exec_mode: 'fork',
       autorestart: true,
@@ -27,7 +27,7 @@ module.exports = {
       repo: 'https://github.com/douglasnobree/SmartMonitorAPI.git',
       path: '/home/jarbas/iot-django',
       'post-deploy':
-        'export PATH=$PATH:/home/jarbas/.virtualenvs/django-env/bin && source /home/jarbas/.virtualenvs/django-env/bin/activate && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && pm2 reload ecosystem.config.js --env production',
+        'export PATH=$PATH:/home/jarbas/.nvm/versions/node/v20.18.1/bin && source /home/jarbas/.virtualenvs/django-env/bin/activate && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && pm2 reload ecosystem.config.js --env production',
     },
   },
 };
