dbaf22e8e1f056c851280ea02a2317b1df2806d5
douglasnobree <douglasclaudino543@gmail.com>
2025-02-16 15:19:01 -0300

    feat: adicionar configuração do ecosystem para gerenciamento de aplicação Django

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: 'django-api',
+      script: 'gunicorn',
+      args: 'config.wsgi:application --bind 0.0.0.0:8000 --workers 3',
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        DJANGO_SETTINGS_MODULE: 'config.settings',
+        ENV: 'development',
+      },
+      env_production: {
+        DJANGO_SETTINGS_MODULE: 'config.settings',
+        ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'jarbas',
+      host: 'smartmonitor.ifce.edu.br',
+      ref: 'origin/main',
+      repo: 'https://github.com/douglasnobree/SmartMonitorAPI.git',
+      path: '/home/jarbas/django-api',
+      'post-deploy':
+        'export PATH=$PATH:/home/jarbas/.virtualenvs/django-env/bin && source /home/jarbas/.virtualenvs/django-env/bin/activate && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
