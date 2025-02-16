6a6f019a4c15cc805038bd008d31fb91445ed50c
douglasnobree <douglasclaudino543@gmail.com>
2025-02-16 16:04:09 -0300

    fix: remover caminho absoluto do gunicorn e corrigir comando pm2 no script de pós-deploy do ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,18 @@ module.exports = {
   apps: [
     {
       name: 'django-api',
-      script: '/home/jarbas/.virtualenvs/django-env/bin/gunicorn', // Caminho absoluto para o gunicorn no ambiente virtual
-      args: 'projectSM.wsgi:application --bind 0.0.0.0:8000 --workers 3 --pythonpath /home/jarbas/iot-django/source',
+      script: '/home/jarbas/.virtualenvs/django-env/bin/gunicorn',
+      args: 'projectSM.wsgi:application --bind 0.0.0.0:8000 --workers 3',
       exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
-        DJANGO_SETTINGS_MODULE: 'projectSM.settings', // Ajustado para refletir o nome correto do módulo de configurações
+        DJANGO_SETTINGS_MODULE: 'projectSM.settings',
         ENV: 'development',
       },
       env_production: {
-        DJANGO_SETTINGS_MODULE: 'projectSM.settings', // Ajustado para refletir o nome correto do módulo de configurações
+        DJANGO_SETTINGS_MODULE: 'projectSM.settings',
         ENV: 'production',
       },
     },
@@ -27,7 +27,7 @@ module.exports = {
       repo: 'https://github.com/douglasnobree/SmartMonitorAPI.git',
       path: '/home/jarbas/iot-django',
       'post-deploy':
-        'export PATH=$PATH:/home/jarbas/.nvm/versions/node/v20.18.1/bin:/home/jarbas/.virtualenvs/django-env/bin && source /home/jarbas/.virtualenvs/django-env/bin/activate && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && /home/jarbas/.nvm/versions/node/v20.18.1/bin/pm2 reload ecosystem.config.js --env production',
+        'export PATH=$PATH:/home/jarbas/.nvm/versions/node/v20.18.1/bin:/home/jarbas/.virtualenvs/django-env/bin && source /home/jarbas/.virtualenvs/django-env/bin/activate && pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput && pm2 reload ecosystem.config.js --env production',
     },
   },
 };
